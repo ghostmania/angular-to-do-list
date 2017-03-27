4803e986efb173daa94aba4a6612b5379de30460
ghostmania <ghostmania2@gmail.com>
2017-03-27 22:22:37 +0300

    added list creation

--- src/main.controller.js
+++ src/main.controller.js
@@ -2,5 +2,12 @@ angular.
     module("myUl",[]). // set module
     controller('MainController', ['$scope', function ($scope) {
         $scope.input = ""; // set empty value to input
+        $scope.lis = [];
+        $scope.addItem = function () { // Add a Item to the list
+            $scope.lis.push({
+                value: $scope.input
+            });
+            $scope.input = ""; // Clear input fields after push
+        };
     }
 ]);
