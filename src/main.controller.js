3141259c68a0ca29fb14cbbe95f29031c800828a
evgeny kirilenko <jinkaz@bk.ru>
2017-03-28 10:40:25 +0300

    added check for empty input value

--- src/main.controller.js
+++ src/main.controller.js
@@ -3,11 +3,16 @@ angular.
     controller('MainController', ['$scope', function ($scope) {
         $scope.input = ""; // set empty value to input
         $scope.lis = [];
-        $scope.addItem = function () { // Add a Item to the list
-            $scope.lis.push({
-                value: $scope.input
-            });
-            $scope.input = ""; // Clear input fields after push
-        };
-    }
+        $scope.addItem = function () { // Add input value to lis
+            if ($scope.input.length){ // check for empty input value
+                $scope.lis.push({
+                    value: $scope.input
+                });
+                $scope.input = ""; // Clear input after push
+                }
+            };
+        $scope.pages = [
+            page = 1
+        ];
+    }//end controller
 ]);
