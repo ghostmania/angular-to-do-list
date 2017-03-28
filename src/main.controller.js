192da6f0aa6b719610a5e55e1ed0aad683d1d404
evgeny kirilenko <jinkaz@bk.ru>
2017-03-28 11:37:20 +0300

    added show only 3 elements on page and pages creation when needed

--- src/main.controller.js
+++ src/main.controller.js
@@ -3,16 +3,26 @@ angular.
     controller('MainController', ['$scope', function ($scope) {
         $scope.input = ""; // set empty value to input
         $scope.lis = [];
+        $scope.pages = [1];
+        $scope.currentPage = 1;
         $scope.addItem = function () { // Add input value to lis
             if ($scope.input.length){ // check for empty input value
                 $scope.lis.push({
                     value: $scope.input
                 });
-                $scope.input = ""; // Clear input after push
+                if (Math.ceil($scope.lis.length / 3) > $scope.pages.length){ // create pages when needed
+                    $scope.pages.push($scope.pages.length + 1)
                 }
-            };
-        $scope.pages = [
-            page = 1
-        ];
+                $scope.input = ""; // Clear input after push
+            }
+        };
+        $scope.addOnEnter = function (e) {
+            if (e.keyCode == 13) {
+                $scope.addItem()
+            }
+        };
+        $scope.showOnCurrentPage = function (index) { // show element when needed
+            return (($scope.currentPage*3 - 3) <= index && index < ($scope.currentPage*3))
+        };
     }//end controller
 ]);
