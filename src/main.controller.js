a7edce24433002401f9f347935b5da2cab01b2a9
evgeny kirilenko <jinkaz@bk.ru>
2017-03-28 17:29:08 +0300

    added checkbox functionality / different view on tabs / code refactoring

--- src/main.controller.js
+++ src/main.controller.js
@@ -2,16 +2,24 @@ angular.
     module("myUl",[]). // set module
     controller('MainController', ['$scope', function ($scope) {
         $scope.input = ""; // set empty value to input
-        $scope.lis = [];
+        $scope.wishes = [];
         $scope.pages = [1];
+        $scope.filteredWishes = [];
         $scope.currentPage = 1;
+        $scope.currentTab = 'all';
         $scope.addItem = function () { // Add input value to lis
             if ($scope.input.length){ // check for empty input value
-                $scope.lis.push({
+                $scope.wishes.push({
                     value: $scope.input,
                     checked: false
                 });
-                if (Math.ceil($scope.lis.length / 3) > $scope.pages.length){ // create pages when needed
+                if ($scope.currentTab !== 'completed') {
+                    $scope.filteredWishes.push({
+                        value: $scope.input,
+                        checked: false
+                    });
+                }
+                if (Math.ceil($scope.filteredWishes.length / 3) > $scope.pages.length) { // create pages when needed
                     $scope.pages.push($scope.pages.length + 1)
                 }
                 $scope.input = ""; // Clear input after push
@@ -31,16 +39,47 @@ angular.
             }
         };
         $scope.removeLi = function (index) { // removeLi function
-            $scope.lis.splice(index, 1);
-            if (Math.ceil($scope.lis.length / 3) < $scope.pages.length && $scope.pages.length > 1) {
+            $scope.wishes.splice(index, 1);
+            $scope.filteredWishes.splice(index, 1);
+            if (Math.ceil($scope.filteredWishes.length / 3) < $scope.pages.length && $scope.pages.length > 1){
                 $scope.pages.splice($scope.pages.length - 1, 1);
                 if ($scope.currentPage > $scope.pages.length) { // if removed last page, redirected to previous
                     $scope.currentPage = $scope.pages.length;
                 }
             }
-        }
-        $scope.changeStatus = function () {
-            console.log($scope.lis);
+        };
+        $scope.showWishes = function (f) {
+            $scope.currentPage = 1;
+            if (f == 'all'){
+                $scope.currentTab = f;
+                $scope.filteredWishes = angular.copy($scope.wishes);
+            } else {
+                f ? $scope.currentTab = 'completed': $scope.currentTab = 'active';
+                $scope.filteredWishes = $scope.wishes.filter(function (li) {
+                    return li.checked == f
+                });
+            }
+            $scope.pages = [1];
+            for (var i=2; i <= (Math.ceil($scope.filteredWishes.length / 3)); i++) { // create pages
+                $scope.pages.push(i)
+            }
+        };
+        $scope.checkUncheck = function (index, li) {
+            var xx = $scope.wishes.find(function (wish) {
+                return wish.value == li.value
+            });
+            console.log(xx);
+            xx.checked = li.checked;
+            console.log(xx);
+            if ($scope.currentTab !== 'all') {
+                $scope.filteredWishes.splice(index, 1);
+                if (Math.ceil($scope.filteredWishes.length / 3) < $scope.pages.length && $scope.pages.length > 1) {
+                    $scope.pages.splice($scope.pages.length - 1, 1);
+                    if ($scope.currentPage > $scope.pages.length) { // if removed last page, redirected to previous
+                        $scope.currentPage = $scope.pages.length;
+                    }
+                }
+            }
         }
     }//end controller
 ]);
