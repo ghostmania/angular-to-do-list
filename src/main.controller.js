f712c59db95b070f11639c0d97a2f31d81e5d60a
ghostmania <ghostmania2@gmail.com>
2017-03-30 16:22:41 +0300

    created to-do component for list

--- src/main.controller.js
+++ src/main.controller.js
@@ -25,25 +25,10 @@ angular.
                 $scope.addItem()
             }
         };
-        $scope.showOnCurrentPage = function (index) { // show element when needed
-            return (($scope.currentPage*3 - 3) <= index && index < ($scope.currentPage*3))
-        };
+
         $scope.goToPage = function (page) { // change page function
             if (page >= 1 && page <= $scope.pages.length) {
                 $scope.currentPage = page;
-            }
-        };
-        $scope.removeLi = function (index, liToDelete) { // removeLi function
-            var neededLi = $scope.wishes.find(function (wish) { // find li in wishes
-                return wish.value == liToDelete.value
-            });
-            $scope.wishes.splice($scope.wishes.indexOf(neededLi), 1); // remove li form wishes
-            var liToShow = $filter('filter')($scope.wishes, { checked: $scope.status });
-            if (Math.ceil(liToShow.length / 3) < $scope.pages.length && $scope.pages.length > 1){
-                $scope.pages.splice($scope.pages.length - 1, 1);
-                if ($scope.currentPage > $scope.pages.length) { // if removed last page, redirected to previous
-                    $scope.currentPage = $scope.pages.length;
-                }
             }
         };
         $scope.showWishes = function (f) {
@@ -51,23 +36,9 @@ angular.
             $scope.status = f;
             $scope.pages = [1];
             var liToShow = $filter('filter')($scope.wishes, { checked: $scope.status });
-            console.log(liToShow);
-            console.log($scope.wishes);
             for (var i=2; i <= (Math.ceil(liToShow.length / 3)); i++) { // recreate pages
                 $scope.pages.push(i)
             }
         };
-        $scope.checkUncheck = function (index) {
-            var liToShow = $filter('filter')($scope.wishes, { checked: $scope.status }); // filter lis
-            if ($scope.status !== '') { // check if not on All tab
-                liToShow.splice(index, 1); // hide li from view
-                if (Math.ceil(liToShow.length / 3) < $scope.pages.length && $scope.pages.length > 1) { // recount pages
-                    $scope.pages.splice($scope.pages.length - 1, 1);
-                    if ($scope.currentPage > $scope.pages.length) { // if removed last page, redirected to previous
-                        $scope.currentPage = $scope.pages.length;
-                    }
-                }
-            }
-        }
     }//end controller
 ]);
---
+++ toDo/toDo.component.js
@@ -0,0 +1,46 @@
+angular.
+    module("myUl").
+    component('toDo',{
+        templateUrl: './toDo/to-do.html',
+        transclude: true,
+        controller: toDoCTRL,
+        bindings: {
+            wishes: '=',
+            currentPage: '=',
+            status: '=',
+            pages: '='
+        }
+});//end component
+
+function toDoCTRL($filter) {
+    var ctrl = this;
+    ctrl.showOnCurrentPage = function (index) { // show element when needed
+        return ((ctrl.currentPage*3 - 3) <= index && index < (ctrl.currentPage*3))
+    };
+    ctrl.checkUncheck = function (index) {
+        var liToShow = $filter('filter')(ctrl.wishes, { checked: ctrl.status }); // filter lis
+        if (ctrl.status !== '') { // check if not on All tab
+            liToShow.splice(index, 1); // hide li from view
+            if (Math.ceil(liToShow.length / 3) < ctrl.pages.length && ctrl.pages.length > 1) { // recount pages
+                ctrl.pages.splice(ctrl.pages.length - 1, 1);
+                if (ctrl.currentPage > ctrl.pages.length) { // if removed last page, redirected to previous
+                    ctrl.currentPage = ctrl.pages.length;
+                }
+            }
+        }
+    };
+    ctrl.removeLi = function (index, liToDelete) { // removeLi function
+        var neededLi = ctrl.wishes.find(function (wish) { // find li in wishes
+            return wish.value == liToDelete.value
+        });
+        ctrl.wishes.splice(ctrl.wishes.indexOf(neededLi), 1); // remove li form wishes
+        var liToShow = $filter('filter')(ctrl.wishes, { checked: ctrl.status });
+        if (Math.ceil(liToShow.length / 3) < ctrl.pages.length && ctrl.pages.length > 1){
+            ctrl.pages.splice(ctrl.pages.length - 1, 1);
+            if (ctrl.currentPage > ctrl.pages.length) { // if removed last page, redirected to previous
+                ctrl.currentPage = ctrl.pages.length;
+            }
+        }
+    };
+
+}//end CTRLfunction
